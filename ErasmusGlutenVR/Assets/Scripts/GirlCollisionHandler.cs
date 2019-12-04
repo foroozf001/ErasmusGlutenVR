7cc5b5360ed3d4d74ee47d385bf447c77d32f143
Faraz Foroozan <farazforoozan@gmail.com>
2019-12-04 21:26:44 +0100

    Chef has collider and plays hit animation if hit by gluten object

---
+++ ErasmusGlutenVR/Assets/Scripts/GirlCollisionHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GirlCollisionHandler : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == null)
+            return;
+
+        if (other.gameObject.GetComponent<EdibleObject>() != null)
+            if (other.gameObject.GetComponent<EdibleObject>().HasGluten())
+                this.GetComponent<CharacterControl>().Hit = true;
+    }
+}
