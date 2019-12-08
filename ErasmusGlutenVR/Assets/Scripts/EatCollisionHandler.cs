f5864c4f60b8a4dde4e720538c36e018780bcceb
Stijn Tromp <stijntromp@chello.nl>
2019-12-08 17:15:28 +0100

    Added code for handling audio sources when eating

--- ErasmusGlutenVR/Assets/Scripts/EatCollisionHandler.cs
+++ ErasmusGlutenVR/Assets/Scripts/EatCollisionHandler.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class EatCollisionHandler : MonoBehaviour
 {
+    public AudioClip eating;
+    AudioSource audioSource;
+
     public ParticleSystem success;
     public ParticleSystem death;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other == null)
@@ -17,6 +26,8 @@ public class EatCollisionHandler : MonoBehaviour
             {
                 OVRGrabber grabber = other.gameObject.GetComponent<OVRGrabbable>().grabbedBy;
                 OVRGrabbable grabbedObject = grabber.grabbedObject;
+
+                audioSource.PlayOneShot(eating, 1f);
 
                 if (grabbedObject.GetComponent<EdibleObject>().HasGluten())
                     death.Play();
