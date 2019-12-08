ba36b306dc94f0c2fee5ce2a862def5d35fbdc16
Ian <ianlippelt@gmail.com>
2019-12-08 12:38:52 +0100

    Added winning and losing to the game
    Win after 3 hits
    Lose after 60 seconds of playtime
    *Currently uses GameObject.Find(), might need to be reworked for performance

---
+++ ErasmusGlutenVR/Assets/RoundResultManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoundResultManager : MonoBehaviour
+{
+    public bool win;
+    public bool lost;
+    private float timeLeft = 60f;
+    GameObject spawner;
+    public int hitCount = 0;
+
+    private void Start()
+    {
+        spawner = GameObject.Find("SpawnerRight");
+    }
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0 && !win)
+        {
+            lost = true;
+            Lost();
+        }
+
+        if (hitCount == 3 && !lost)
+        {
+            win = true;
+            Win();
+        }
+    }
+
+    private void Lost()
+    {
+        spawner.SetActive(false);
+        GetComponentInChildren<Text>().text = "Game Over";
+    }
+
+    private void Win()
+    {
+        spawner.SetActive(false);
+        GetComponentInChildren<Text>().text = "You Win!";
+    }
+}
