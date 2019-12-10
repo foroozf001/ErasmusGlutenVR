c85550ea20a4f9b7cdbb02e42697194972e9d7e1
Ian <ianlippelt@gmail.com>
2019-12-10 20:56:27 +0100

    Added a timer with a round visual clock hooked to it aswell

--- ErasmusGlutenVR/Assets/RoundResultManager.cs
+++ ErasmusGlutenVR/Assets/RoundResultManager.cs
@@ -8,17 +8,25 @@ public class RoundResultManager : MonoBehaviour
     public bool win;
     public bool lost;
     private float timeLeft = 60f;
+    private float maxTime;
     GameObject spawner;
     public int hitCount = 0;
+    public Image timer;
 
     private void Start()
     {
         spawner = GameObject.Find("SpawnerRight");
+        maxTime = timeLeft;
+        timer = GameObject.Find("ForegroundTimer").GetComponent<Image>();
     }
 
     void Update()
     {
-        timeLeft -= Time.deltaTime;
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+        }
+
         if (timeLeft < 0 && !win)
         {
             lost = true;
@@ -30,6 +38,12 @@ public class RoundResultManager : MonoBehaviour
             win = true;
             Win();
         }
+
+        fillTimer();
+        GameObject.Find("Time").GetComponent<Text>().text = timeLeft.ToString("0.00");
+
+
+
     }
 
     private void Lost()
@@ -42,5 +56,12 @@ public class RoundResultManager : MonoBehaviour
     {
         spawner.SetActive(false);
         GetComponentInChildren<Text>().text = "You Win!";
+    }
+
+    private void fillTimer()
+    {
+        float fill = timeLeft / maxTime;
+        timer.fillAmount = fill;
+
     }
 }
