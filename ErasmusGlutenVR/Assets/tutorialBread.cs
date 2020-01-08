693211062f046a12ad45b145bf216d7fb01a715d
Stijn Tromp <stijntromp@chello.nl>
2020-01-08 21:48:49 +0100

    Bread spawns and get back on the table when target is missed. When target is hit, the bread and target will disappear.

--- ErasmusGlutenVR/Assets/tutorialBread.cs
+++ ErasmusGlutenVR/Assets/tutorialBread.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class tutorialBread : MonoBehaviour
 {
+    Rigidbody RB;
+    [SerializeField] GameObject Target;
     // Start is called before the first frame update
     void Start()
     {
-        
+        RB = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "TutorialGroundCollider")
+        {
+            this.transform.position = new Vector3(-0.2f, 1.1f, -0.2f);
+            RB.velocity = Vector3.zero;
+        }
+        
+        if(collision.gameObject.name == "TutorialTarget")
+        {
+            this.gameObject.SetActive(false);
+            Target.gameObject.SetActive(false);
+            //TODO:SFX
+            //TODO:Text to instruct to wash hands
+        }
     }
 }
