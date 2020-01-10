e32cb293e4a103b65b52e1b739b4f33e25130e91
Faraz Foroozan <farazforoozan@gmail.com>
2020-01-10 21:46:41 +0100

    Reorganised maps and start on intro sequence

---
+++ ErasmusGlutenVR/Assets/_MainFolder/Scripts/HeadTracker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ErasmusGluten
+{
+    public class HeadTracker : MonoBehaviour
+    {
+        public GameObject CameraRig;
+
+        private void Update()
+        {
+            this.transform.position = CameraRig.transform.position;
+        }
+    }
+}
